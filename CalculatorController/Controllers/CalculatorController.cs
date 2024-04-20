4680bd327d1652ecfbd79e9357616c6340be7d34
Trey Morris <trey@morris2morris.com>
2024-04-20 16:21:12 -0400

    Working On CalculatorController

---
+++ CalculatorController/Controllers/CalculatorController.cs
@@ -0,0 +1,24 @@
+﻿using CalculatorEngine;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorController.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class CalculatorController : ControllerBase
+	{
+		private readonly CalculatorEngineMethods engine;
+		public CalculatorController(CalculatorEngineMethods calculatorEngineMethods)
+		{
+			engine = calculatorEngineMethods;
+		}
+
+		[HttpGet("add")]
+		public IActionResult Add(double firstNumber, double secondNumber)
+		{
+			var result = CalculatorEngineMethods.Add(firstNumber, secondNumber);
+			return Ok(result);
+		}
+
+	}
+}

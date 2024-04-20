f30a4c380cfa18adf840ff255344dd922339d4d8
Trey Morris <trey@morris2morris.com>
2024-04-19 22:14:27 -0400

    Update Directory Name

--- CalculatorEngineUnitTests/obj/Debug/net8.0/CalculatorEngineUnitTests.AssemblyInfo.cs
+++ CalculatorEngineUnitTests/obj/Debug/net8.0/CalculatorEngineUnitTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorEngineUnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32906ded20ec6a24eed6763870134767ea850e52")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorEngineUnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorEngineUnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]

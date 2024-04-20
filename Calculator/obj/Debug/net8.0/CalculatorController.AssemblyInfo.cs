397f7551cc9f04271834d7ac4e2182f0be25a51a
Trey Morris <trey@morris2morris.com>
2024-04-19 22:09:40 -0400

    Update Directories

--- Calculator/obj/Debug/net8.0/CalculatorController.AssemblyInfo.cs
+++ Calculator/obj/Debug/net8.0/CalculatorController.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorController")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32906ded20ec6a24eed6763870134767ea850e52")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorController")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorController")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- CalculatorEngine/obj/Debug/net8.0/CalculatorEngine.AssemblyInfo.cs
+++ CalculatorEngine/obj/Debug/net8.0/CalculatorEngine.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CalculatorEngine")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32906ded20ec6a24eed6763870134767ea850e52")]
 [assembly: System.Reflection.AssemblyProductAttribute("CalculatorEngine")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CalculatorEngine")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
